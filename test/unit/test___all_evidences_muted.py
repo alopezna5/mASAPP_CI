bca4fa79e1557c1882b3e6e1356819705349722a
telugu <alopezgn@gmail.com>
2020-09-30 22:32:21 +0200

    fixing tests that was failing

--- test/unit/test___all_evidences_muted.py
+++ test/unit/test___all_evidences_muted.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import unittest
 
 from masappcli.masappcli import mASAPP_CI
@@ -11,30 +13,30 @@ class TestAllEvidencesMuted(unittest.TestCase):
 
     def test_none_element_sent(self):
         input_data = None
-        expected_message = "[X] {} not allowed. Waiting for a vulnerability or behavior JSON".format(input_data)
+        expected_message = "\[X\] {} not allowed. Waiting for a vulnerability or behavior JSON".format(input_data)
 
-        with self.assertRaisesRegexp(ValueError, expected_message):
+        with self.assertRaisesRegex(ValueError, expected_regex=expected_message):
             mASAPP_CI.all_evidences_muted(self.usr, element=input_data)
 
     def test_empty_json_element_sent(self):
         input_data = {}
-        expected_message = "[X] {} not allowed. Waiting for a vulnerability or behavior JSON".format(input_data)
+        expected_message = "\[X\] The mASAPP response has not the expected structure"
 
-        with self.assertRaisesRegexp(ValueError, expected_message):
+        with self.assertRaisesRegex(ValueError, expected_message):
             mASAPP_CI.all_evidences_muted(self.usr, element=input_data)
 
     def test_wrong_type_element_sent(self):
         input_data = "TestingString"
-        expected_message = "[X] {} not allowed. Waiting for a vulnerability or behavior JSON".format(input_data)
+        expected_message = "\[X\] {} not allowed. Waiting for a vulnerability or behavior JSON".format(input_data)
 
-        with self.assertRaisesRegexp(ValueError, expected_message):
+        with self.assertRaisesRegex(ValueError, expected_message):
             mASAPP_CI.all_evidences_muted(self.usr, element=input_data)
 
     def test_result_field_not_sent_in_element(self):
         input_data = json.loads(open("test/data/evidence_without_result_field.json").read())
-        expected_message = "[X] The mASAPP response has not the expected structure. Missing field - result"
+        expected_message = "\[X\] The mASAPP response has not the expected structure"
 
-        with self.assertRaisesRegexp(ValueError, expected_message):
+        with self.assertRaisesRegex(ValueError, expected_message):
             mASAPP_CI.all_evidences_muted(self.usr, element=input_data)
 
     def test_muted_field_not_sent_in_element(self):
@@ -55,5 +57,5 @@ class TestAllEvidencesMuted(unittest.TestCase):
         self.assertFalse(mASAPP_CI.all_evidences_muted(self.usr, element=input_data))
 
     def test_all_muted_evidences_in_element(self):
-        input_data = json.loads(open("test/data/evidence_without_muted_fields.json").read())
+        input_data = json.loads(open("test/data/evidence_with_all_muted_evidences.json").read())
         self.assertTrue(mASAPP_CI.all_evidences_muted(self.usr, element=input_data))
